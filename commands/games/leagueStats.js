e4d4e10b7ce4ba3fdaa24671f64f366d3940e656
joshdfuqua <joshdfuqua@gmail.com>
2022-01-23 02:24:07 -0500

    Updates: Set up check within banana command to prevent bot from responding to itself Added comments to the lolstats command

--- commands/games/leagueStats.js
+++ commands/games/leagueStats.js
@@ -8,7 +8,8 @@ module.exports = {
 	name: 'lolstats',
   description: 'View general stats from your previous 15 LoL matches',
 	execute(message, args, client) {
-    message.channel.sendTyping()
+    message.channel.sendTyping(); // Used to allow bot to retrieve data from API calls while also showing user their input is being worked on
+
     let summonerName = args.join(' ');
 
     // Request PUUID from API based on inputted summoner name
@@ -21,68 +22,77 @@ module.exports = {
       let summonerIcon = response.data.profileIconId;
 
       // Request 15 most recent matches based on received PUUID
+      // TODO: Eventually want to try and upgrade API token to production status to allow for more req/sec & req/2min
+      // Currently capped at 20/sec & 100/2min
       axios({
         method: 'GET',
         url: `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=15`
       })
       .then(response => {
+        // Stats currently being tracked by bot
         let kills = 0,
           deaths = 0,
           assists = 0,
           totalGold = 0,
+          visionScore = 0,
           dblKills = 0,
           tplKills = 0,
           qdaKills = 0,
           ptaKills = 0;
+        // champs obj is used to keep track of frequency of summoner's recently played champions
+        // Will be used later to retrieve champ thumbnail from API
         let champs = {};
 
         let promiseArr = [];
+        // Creating promises here to allow for async code execution.  Need to retrieve data for all 15 matches before sending data back to user.
         let matchPromise = matchID => {
-            return new Promise((resolve, reject) => {
-              // Request match data based on passed in matchID
-              axios({
-                method: 'GET',
-                url: `https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}`
-              })
-              .then(match => {
-                let players = match.data.info.participants;
+          return new Promise((resolve, reject) => {
+            // Request match data based on passed in matchID
+            axios({
+              method: 'GET',
+              url: `https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}`
+            })
+            .then(match => {
+              let players = match.data.info.participants;
 
-                players.forEach(player => {
-                  if (player.puuid === puuid) {
-                    kills += player.kills;
-                    deaths += player.deaths;
-                    assists += player.assists;
-                    totalGold += player.goldEarned;
-                    dblKills += player.doubleKills;
-                    tplKills += player.tripleKills;
-                    qdaKills += player.quadraKills;
-                    ptaKills += player.pentaKills;
+              // Iterates over participants array to find and store data relevant to the searched-for summoner
+              players.forEach(player => {
+                if (player.puuid === puuid) {
+                  kills += player.kills;
+                  deaths += player.deaths;
+                  assists += player.assists;
+                  totalGold += player.goldEarned;
+                  visionScore += player.visionScore;
+                  dblKills += player.doubleKills;
+                  tplKills += player.tripleKills;
+                  qdaKills += player.quadraKills;
+                  ptaKills += player.pentaKills;
 
-                    if (champs[player.championName] === undefined) {
-                      champs[player.championName] = {
-                        count: 1,
-                        timestamp: match.data.info.gameEndTimestamp
-                      }
-                    } else {
-                      champs[player.championName].count += 1;
-                      champs[player.championName].timestamp = (match.data.info.gameEndTimestamp > champs[player.championName].timestamp) ?
-                        match.data.info.gameEndTimestamp :
-                        champs[player.championName].timestamp;
+                  if (champs[player.championName] === undefined) {
+                    champs[player.championName] = {
+                      count: 1,
+                      timestamp: match.data.info.gameEndTimestamp
                     }
+                  } else {
+                    champs[player.championName].count += 1;
+                    champs[player.championName].timestamp = (match.data.info.gameEndTimestamp > champs[player.championName].timestamp) ?
+                      match.data.info.gameEndTimestamp :
+                      champs[player.championName].timestamp;
                   }
-                })
+                }
               })
-              .then(() => {
-                resolve()
-              })
-              .catch(err => {
-                reject(err)
-              });
+            })
+            .then(() => {
+              resolve();
+            })
+            .catch(err => {
+              reject(err);
             });
+          });
         };
 
         response.data.forEach(matchID => {
-          promiseArr.push(matchPromise(matchID))
+          promiseArr.push(matchPromise(matchID));
         });
 
         Promise.all(promiseArr)
@@ -94,10 +104,13 @@ module.exports = {
               timestamp: 0
             };
             for (let champ in champs) {
+              // If section of conditional checks for single champ with highest frequency of use
               if (champs[champ].count > favChamp.count) {
                 favChamp.name = champ;
                 favChamp.count = champs[champ].count;
                 favChamp.timestamp = champs[champ].timestamp;
+              // If current champ in iteration has same frequency as champ in favChamp, checks timestamps of both
+              // Timestamps reflect how recent the champ was played
               } else if (champs[champ].count === favChamp.count && champs[champ].timestamp > favChamp.timestamp) {
                 favChamp.name = champ;
                 favChamp.count = champs[champ].count;
@@ -116,7 +129,7 @@ module.exports = {
                 {name: 'Deaths', value: `${deaths}`, inline: true},
                 {name: 'Assists', value: `${assists}`, inline: true},
                 {name: '\u200B', value: '\u200B'},
-                {name: 'KD Ratio', value: `${(kills / deaths).toFixed(2)}`},
+                {name: 'KDA Ratio', value: `${((kills + assists) / deaths).toFixed(2)}`},
                 {name: '\u200B', value: '-- Multi-Kills --------------------------------'},
                 {name: 'Doubles', value: `${dblKills}`, inline: true},
                 {name: 'Triples', value: `${tplKills}`, inline: true},
@@ -124,19 +137,23 @@ module.exports = {
                 {name: 'Quadras', value: `${qdaKills}`, inline: true},
                 {name: 'Pentas', value: `${ptaKills}`, inline: true},
                 {name: '\u200B', value: '-- Misc ---------------------------------------'},
-                {name: 'Total Gold', value: `${totalGold}`},
+                {name: 'Total Gold', value: `${totalGold}`, inline: true},
+                {name: 'Vision Score', value: `${visionScore}`, inline: true}
               )
-              .setFooter({text: 'Data obtained via Riot API; aggregated by The Ape', iconURL: client.user.displayAvatarURL()})
+              .setFooter({text: 'Data obtained via Riot API; aggregated by The Ape', iconURL: client.user.displayAvatarURL()});
 
-            message.channel.send({content: null, embeds: [reply]})
+            message.channel.send({content: null, embeds: [reply]});
           })
       })
     })
     .catch(err => {
       console.log(err);
-      switch (err.response.statusText) {
-        case 'Not Found':
-          message.channel.send({content: 'That character doesn\'t exist!'})
+      switch (err.response.status) {
+        case 404:
+          message.channel.send({content: 'That character doesn\'t exist!'});
+          break;
+        case 429:
+          message.channel.send({content: 'Riot\'s not talking to me right now.  Try again in two minutes.'});
           break;
       }
     })
--- commands/misc/banana.js
+++ commands/misc/banana.js
@@ -1,7 +1,9 @@
 module.exports = {
   name: 'banana',
   description: 'banana',
-  execute(message, args) {
+  execute(message, args, client) {
+    if (message.author.id === client.user.id) return;
+
     message.react('🍌');
     message.channel.send({content: 'where banana', tts: true})
   },
