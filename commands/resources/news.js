a14ba851515b81fbf7b637cc08532f6bd2338086
joshdfuqua <79943156+joshdfuqua@users.noreply.github.com>
2022-09-28 14:13:32 -0400

    Update news.js

--- commands/resources/news.js
+++ commands/resources/news.js
@@ -20,7 +20,6 @@ const savedHtmlLocation =
   ENVIRONMENT === "Local" ? localHtmlPath : serverHtmlPath;
 
 function getNews(client) {
-  console.log(client);
   client.channels
     .fetch("977325210376208415")
     .then((channel) => {
