21d10e0e7c7140bfe113c8522f9e37da769079fe
joshdfuqua <joshdfuqua@gmail.com>
2024-06-04 22:26:59 -0400

    Update game_sales.js
    Forgot to commit work incrementally, so there are a few things happening here lol
    
    Most recent accomplishment is users can now successfully add games to the watchlist

--- commands/games/game_sales.js
+++ commands/games/game_sales.js
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import axios from 'axios';
-import { ComponentType, EmbedBuilder } from 'discord.js';
+import { ComponentType } from 'discord.js';
 import {
 	ButtonStyle,
 	ModalBuilder,
@@ -14,46 +14,42 @@ import {
 } from 'discord.js';
 
 dotenv.config();
-const dummyQueryResponse = [{ external: 'Maybe', gameID: '1234' }];
-
 
 export const data = new SlashCommandBuilder()
 	.setName('gamesales')
 	.setDescription('Add or remove games from the game sale watchlist.');
 
 export async function execute(interaction) {
-	const collectorFilter = i => i.user.id === interaction.user.id;
+	const userFilter = i => i.user.id === interaction.user.id;
 
 	const MainMenu = createMainMenu();
 	const sendMenu = await interaction.reply({
 		components: [MainMenu],
 	});
-	const menuResponse = await sendMenu.awaitMessageComponent({ filter: collectorFilter });
+	const menuResponse = await sendMenu.awaitMessageComponent({ filter: userFilter });
 
 	if (menuResponse.customId === 'add_game') {
 		await menuResponse.showModal(createAddGameModal());
 
-		const modalResponse = await interaction.awaitModalSubmit({ time: 30_000 });
+		const modalResponse = await interaction.awaitModalSubmit({ time: 30000 });
 		const gameTitle = modalResponse.fields.getTextInputValue('game_title');
 		modalResponse.reply({ content: 'Looking through my list of games...', ephemeral: true });
 		modalResponse.deleteReply();
 
 		try {
-			const { data: games } = await queryCheapShark(gameTitle);
-
-			let gamesListSelect;
+			const { data: cheapSharkGames } = await queryCheapSharkGames(gameTitle);
 			let sendGamesList;
-			if (games.length <= 25) {
-				gamesListSelect = createGamesListSelect(games);
+			if (cheapSharkGames.length <= 25) {
+				const gamesListSelect = createGamesListSelect(cheapSharkGames);
 				sendGamesList = await interaction.editReply({ components: gamesListSelect });
 			}
 			// Handles pagination
 			else {
 				let currentIndex = 0;
-				gamesListSelect = createGamesListSelect(games.slice(0, 25));
+				let gamesListSelect = createGamesListSelect(cheapSharkGames);
 				sendGamesList = await interaction.editReply({ components: gamesListSelect });
 
-				const buttonWatcher = sendGamesList.createMessageComponentCollector({ ComponentType: ComponentType.Button, time: 180000 });
+				const buttonWatcher = sendGamesList.createMessageComponentCollector({ componentType: ComponentType.Button, time: 180000 });
 				buttonWatcher.on('collect', async i => {
 					i.deferUpdate();
 
@@ -63,25 +59,27 @@ export async function execute(interaction) {
 					else {
 						currentIndex -= 25;
 					}
-
-					const canNavigateBack = games[currentIndex - 25] !== undefined;
-					const canNavigateForward = games[currentIndex + 25] !== undefined;
-					const currentGamesListSegment = games.slice(currentIndex, currentIndex + 25);
-					console.log(canNavigateForward);
-					gamesListSelect = createGamesListSelect(currentGamesListSegment, canNavigateBack, canNavigateForward);
+					gamesListSelect = createGamesListSelect(cheapSharkGames, currentIndex);
 					sendGamesList = await interaction.editReply({ components: gamesListSelect });
 				});
 			}
 
-			const selectResponse = await sendGamesList.awaitMessageComponent({ filter: collectorFilter });
+			const selectResponse = await sendGamesList.awaitMessageComponent({ filter: (i) => i.customId === 'games_list' && userFilter(i) });
+			const gameID = selectResponse.values[0];
 
-			const gameInDatabase = await checkDatabase(selectResponse.values[0]);
+			const gameInDatabase = await checkDatabase(gameID);
 
 			selectResponse.reply({ content: 'Checking to see if we already have this one...', ephemeral: true });
 			selectResponse.deleteReply();
 
 			if (gameInDatabase) {
 				interaction.editReply({ content: 'That game is already being tracked!', components: [] });
+			}
+			else {
+				const [ gameInfo ] = cheapSharkGames.filter(g => g.gameID === gameID);
+				const saveSuccess = await saveToDatabase(gameInfo);
+				const responseMessage = saveSuccess ? 'Game is now being tracked.' : 'It seems I had a problem saving the game.  Try again later.';
+				interaction.editReply({ content: responseMessage, components: [] });
 			}
 		}
 		catch (e) {
@@ -94,24 +92,39 @@ export async function execute(interaction) {
 	}
 }
 
-const queryCheapShark = async (title) => {
-	return axios({
-		method: 'GET',
-		url: `https://www.cheapshark.com/api/1.0/games?title=${title}`,
-	});
+/* ********* */
+/* API Calls */
+/* ********* */
+const queryCheapSharkGames = async (title) => {
+	return axios.get(`https://www.cheapshark.com/api/1.0/games?title=${title}`);
 };
 
-const checkDatabase = async (title) => {
-	const request = await axios.get(
+const checkDatabase = async (gameID) => {
+	const response = await axios.get(
+		'http://localhost:3001/api/gamesales',
+		{
+			params: {
+				gameID,
+			},
+			validateStatus: false,
+		});
+
+	return response.status === 200;
+};
+
+const saveToDatabase = async ({ gameID, external: title }) => {
+	const response = await axios.post(
 		'http://localhost:3001/api/gamesales',
 		null,
 		{
 			params: {
+				gameID,
 				title,
 			},
+			validateStatus: false,
 		});
 
-	return request.status === 200;
+	return response.status === 201;
 };
 
 /* ********** */
@@ -148,9 +161,30 @@ const createAddGameModal = () => {
 	return modal;
 };
 
-const createGamesListSelect = (games, canNavigateBack = false, canNavigateForward = true) => {
-	const buttonContainer = new ActionRowBuilder();
+const createGamesListSelect = (games, currentIndex = 0) => {
+	const currentGamesListSection = games.slice(currentIndex, currentIndex + 25);
+
 	const selectContainer = new ActionRowBuilder();
+	const gamesSelect = new StringSelectMenuBuilder()
+		.setCustomId('games_list')
+		.setPlaceholder('Select the correct title');
+	const gamesSelectOptions = [];
+
+	currentGamesListSection.forEach(({ gameID, external: title }) => {
+		gamesSelectOptions.push(
+			new StringSelectMenuOptionBuilder()
+				.setLabel(title)
+				.setValue(gameID),
+		);
+	});
+	gamesSelect.addOptions(gamesSelectOptions);
+	selectContainer.addComponents(gamesSelect);
+
+	if (games.length <= 25) return [ selectContainer ];
+
+	const canNavigateBack = games[currentIndex - 25] !== undefined;
+	const canNavigateForward = games[currentIndex + 25] !== undefined;
+	const buttonContainer = new ActionRowBuilder();
 	const prevButton = new ButtonBuilder()
 		.setCustomId('previous')
 		.setLabel('<-')
@@ -161,22 +195,7 @@ const createGamesListSelect = (games, canNavigateBack = false, canNavigateForwar
 		.setLabel('->')
 		.setStyle(ButtonStyle.Secondary)
 		.setDisabled(!canNavigateForward);
-	const gamesList = new StringSelectMenuBuilder()
-		.setCustomId('games_list')
-		.setPlaceholder('Select the correct title');
-	const gamesListOptions = [];
-
-	games.forEach(({ external: title }) => {
-		gamesListOptions.push(
-			new StringSelectMenuOptionBuilder()
-				.setLabel(title)
-				.setValue(title),
-		);
-	});
-	gamesList.addOptions(gamesListOptions);
-
 	buttonContainer.addComponents(prevButton, nextButton);
-	selectContainer.addComponents(gamesList);
 
-	return [ selectContainer, buttonContainer ];
+	return [ selectContainer, buttonContainer ] ;
 };
