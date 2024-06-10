2312d0245b600a0e4394481b6aaa442525010f40
joshdfuqua <joshdfuqua@gmail.com>
2024-06-10 00:06:45 -0400

    Update GameSales controller
    - Update to account for server removal

--- database/controllers/GameSales.js
+++ database/controllers/GameSales.js
@@ -1,50 +1,42 @@
-import service from '../services/GameSales.js';
+import * as service from '../services/GameSales.js';
 
-const handleGet = async (req, res) => {
-	const { cheapSharkId } = req.query;
-
-	if (cheapSharkId) {
-		try {
-			const game = await service.findGame(cheapSharkId);
-			res.status(200).json(game);
-		}
-		catch (err) {
-			res.status(500).json({ error: err.message });
-		}
-	}
-	else {
-		try {
-			const games = await service.retrieveAll();
-			res.status(200).json(games);
-		}
-		catch (err) {
-			res.status(500).json({ error: err.message });
-		}
-	}
-};
-const handlePost = async (req, res) => {
-	const { cheapSharkId, title } = req.query;
+export const findGame = async (cheapSharkId) => {
 	try {
-		await service.addGame(cheapSharkId, title);
-		res.status(201).end();
+		const game = await service.findGame(cheapSharkId);
+		return game;
 	}
 	catch (err) {
-		res.status(500).json({ error: err.message });
-	}
-};
-const handleDelete = async (req, res) => {
-	const { cheapSharkId } = req.query;
-	try {
-		await service.removeGame(cheapSharkId);
-		res.status(200).end();
-	}
-	catch (err) {
-		res.status(500).json({ error: err.message });
+		return err;
 	}
 };
 
-export default {
-	handleGet,
-	handlePost,
-	handleDelete,
+export const retrieveAll = async () => {
+	try {
+		const games = await service.retrieveAll();
+		return games;
+	}
+	catch (err) {
+		return err;
+	}
 };
+
+export const addGame = async (cheapSharkId, title) => {
+	try {
+		const databaseResponse = await service.addGame(cheapSharkId, title);
+		console.log(databaseResponse);
+		return databaseResponse;
+	}
+	catch (err) {
+		return err;
+	}
+};
+export const removeGame = async (cheapSharkId) => {
+	try {
+		const databaseResponse = await service.removeGame(cheapSharkId);
+		return databaseResponse;
+	}
+	catch (err) {
+		return err;
+	}
+};
+
