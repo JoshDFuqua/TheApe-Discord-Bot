03ee9c08de66397e64a7710c44c9661310f2d9ad
joshdfuqua <joshdfuqua@gmail.com>
2025-10-17 13:57:20 -0400

    Remove legacy code

--- commands/misc/banana.js
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder()
-	.setName('banana')
-	.setDescription('banana');
-export async function execute(interaction) {
-	interaction.reply({ content: 'where banana', tts: true });
-}
--- commands/misc/say.js
+++
@@ -1,7 +0,0 @@
-export const name = 'say';
-export const description = 'Make the bot say something';
-export function execute(message, args) {
-	const response = [...args].join(' ');
-	message.delete();
-	message.channel.send({ content: `${response}` });
-}
